45c65d9bf111dcf7db3b86abfce26b55470c6676
fei.han <arfly33@gmail.com>
2022-09-20 00:35:58 +0800

    feature: 初始化背景样式和图片

--- App.tsx
+++ App.tsx
@@ -8,113 +8,98 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {Text, View, Image} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const plateImg = require('./assets/plate.png');
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const containerStyle = {
+    backgroundColor: '#1E1E1E',
+    flex: 1,
+    marginTop: 47,
+    marginBottom: 34,
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const headerStyle = {
+    height: 73,
+    backgroundColor: '#FD003C',
+    position: 'absolute',
+    width: '100%',
+  };
+
+  const bodyStyle = {
+    position: 'absolute',
+    top: 120,
+    flex: 1,
+  };
+
+  const footerStyle = {
+    height: 65,
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  };
+
+  const plateStyle = {
+    position: 'absolute',
+    boxSizing: 'border-box',
+    width: 256.63,
+    height: 97.11,
+    left: 59,
+    top: 478,
+
+    // background: linear-gradient(179.1deg, #FF2F57 -35.41%, #FF5977 99.32%);
+    // border: 2px solid #FF5F7D;
+    // box-shadow: 0px 3px 2px rgba(189, 68, 68, 0.447124), inset 0px 3px 15px rgba(179, 0, 0, 0.593835);
+    // border-radius: 13px;
+  };
+
+  const paybuttonStyle = {
+    width: 143,
+    height: 65,
+    position: 'absolute',
+    right: 0,
+  };
+
+  const paytextStyle = {
+    position: 'absolute',
+    width: (143 - 57) / 2,
+    height: (65 - 27) / 2,
+    left: 10,
+    top: 30,
+
+    fontFamily: 'PingFang SC',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 32,
+    lineHeight: 32 * 1.02,
+    /* or 50% */
+
+    color: '#FFFFFF',
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <LinearGradient
+      colors={['#F5F5F5', '#FFEDED']}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      style={containerStyle}>
+      <LinearGradient colors={['#FD003C', '#FF5D79']} style={headerStyle}>
+        <Text>Header</Text>
+      </LinearGradient>
+      <View style={bodyStyle}>
+        <Image source={plateImg} style={plateStyle} />
+      </View>
+      <View style={footerStyle}>
+        <LinearGradient colors={['#FD003C', '#FF5D79']} style={paybuttonStyle}>
+          <Text style={paytextStyle}>Pay</Text>
+        </LinearGradient>
+      </View>
+    </LinearGradient>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
