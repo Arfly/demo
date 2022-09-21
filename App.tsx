a06687f1836286d883866d5e205b39241d945965
fei.han <arfly33@gmail.com>
2022-09-22 01:49:42 +0800

    feature: 加入购物车动画

--- App.tsx
+++ App.tsx
@@ -1,23 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import {Text, View, Image, Dimensions, StyleSheet, Alert} from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  Dimensions,
+  StyleSheet,
+  Animated,
+  TouchableOpacity,
+} from 'react-native';
 import {SceneMap, TabView} from 'react-native-tab-view';
 
-const plateImg = require('./assets/plate.png');
-const suggestImg = require('./assets/suggest.png');
-const burgerImg = require('./assets/burger.png');
-const cupImg = require('./assets/cup.png');
-const eatImg = require('./assets/eat.png');
+const IMG = {
+  plate: require('./assets/plate.png'),
+  suggest: require('./assets/suggest.png'),
+  burger: require('./assets/burger.png'),
+  cup: require('./assets/cup.png'),
+  eat: require('./assets/eat.png'),
+};
 
 const styles = StyleSheet.create({
   header: {
@@ -127,8 +127,8 @@ const styles = StyleSheet.create({
     fontSize: 32,
     lineHeight: 36,
     color: '#EB5C77',
-    right: 70,
-    top: 80,
+    right: 40,
+    top: 100,
     position: 'absolute',
   },
   price: {
@@ -139,13 +139,13 @@ const styles = StyleSheet.create({
     lineHeight: 28,
     color: '#EB5C77',
     position: 'absolute',
-    right: 70,
-    top: 112,
+    right: 48,
+    top: 142,
   },
   addButton: {
     position: 'absolute',
-    width: 59,
-    height: 59,
+    width: 99,
+    height: 99,
     left: 268,
     top: 298,
   },
@@ -158,6 +158,12 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height / 2 - 73,
     marginTop: 73,
   },
+  food: {
+    position: 'absolute',
+    left: 260,
+    top: 270,
+    display: 'none',
+  },
 });
 
 function Fries() {
@@ -165,27 +171,52 @@ function Fries() {
     container: {
       position: 'relative',
     },
-    upper: {
+    upper1: {
+      position: 'absolute',
+      top: -50,
+      left: 40,
+    },
+    upper2: {
       position: 'absolute',
       top: 0,
-      left: 0,
+      left: -10,
     },
     main: {
       position: 'absolute',
-      top: 0,
+      top: 25,
       left: 0,
+      width: 200,
+      height: 200,
+    },
+    star1: {
+      position: 'absolute',
+      top: 25,
+      left: 160,
+      transform: [{scale: 0.7}],
+    },
+    star2: {
+      position: 'absolute',
+      top: 90,
+      left: 180,
+      transform: [{scale: 0.3}],
+    },
+    star3: {
+      position: 'absolute',
+      top: 90,
+      left: 0,
+      transform: [{scale: 0.3}],
     },
   });
   return (
     <View style={[friesStyles.container, styles.scrollViewContainer]}>
-      <Image
-        source={require('./assets/shutiao.png')}
-        style={friesStyles.main}
-      />
-      <Image source={require('./assets/st1.png')} style={friesStyles.upper} />
-      <Image source={require('./assets/st2.png')} style={friesStyles.upper} />
+      <Image style={friesStyles.star1} source={require('./assets/star1.png')} />
+      <Image style={friesStyles.star2} source={require('./assets/star1.png')} />
+      <Image style={friesStyles.star3} source={require('./assets/star1.png')} />
+      <Image source={require('./assets/st.png')} style={friesStyles.main} />
+      <Image source={require('./assets/st1.png')} style={friesStyles.upper1} />
+      <Image source={require('./assets/st2.png')} style={friesStyles.upper2} />
       <Text style={styles.foodName}>FRIES</Text>
-      <Text style={styles.price}>4$</Text>
+      <Text style={styles.price}>4 $</Text>
     </View>
   );
 }
@@ -197,21 +228,53 @@ function Coffee() {
     },
     upper: {
       position: 'absolute',
-      top: 0,
-      left: 0,
+      top: -40,
+      left: 5,
     },
     main: {
       position: 'absolute',
       top: 0,
       left: 0,
+      width: 243,
+      height: 258,
+    },
+    star1: {
+      position: 'absolute',
+      top: 60,
+      left: 160,
+      transform: [{scale: 0.3}],
+    },
+    star2: {
+      position: 'absolute',
+      top: 170,
+      left: 140,
+      transform: [{scale: 0.8}],
+    },
+    star3: {
+      position: 'absolute',
+      top: 180,
+      left: 20,
+      transform: [{scale: 0.5}],
     },
   });
   return (
     <View style={[coffeeStyles.container, styles.scrollViewContainer]}>
+      <Image
+        style={coffeeStyles.star1}
+        source={require('./assets/star1.png')}
+      />
+      <Image
+        style={coffeeStyles.star2}
+        source={require('./assets/star1.png')}
+      />
+      <Image
+        style={coffeeStyles.star3}
+        source={require('./assets/star1.png')}
+      />
       <Image source={require('./assets/cf2.png')} style={coffeeStyles.main} />
       <Image source={require('./assets/cf1.png')} style={coffeeStyles.upper} />
       <Text style={styles.foodName}>LATTE</Text>
-      <Text style={styles.price}>3$</Text>
+      <Text style={styles.price}>3 $</Text>
     </View>
   );
 }
@@ -224,30 +287,78 @@ function Burger() {
     upper: {
       position: 'absolute',
       top: 0,
-      left: 0,
+      left: 30,
     },
     main: {
       position: 'absolute',
-      top: 0,
-      left: 0,
+      top: 60,
+      left: 30,
+      width: 200,
+      height: 200,
+    },
+    star1: {
+      position: 'absolute',
+      top: 10,
+      left: 25,
+      transform: [{scale: 0.3}],
+    },
+    star2: {
+      position: 'absolute',
+      top: 230,
+      left: 180,
+      transform: [{scale: 0.3}],
+    },
+    star3: {
+      position: 'absolute',
+      top: 190,
+      left: 25,
+      transform: [{scale: 0.5}],
     },
   });
   return (
     <View style={[burgerStyles.container, styles.scrollViewContainer]}>
+      <Image
+        style={burgerStyles.star1}
+        source={require('./assets/star1.png')}
+      />
+      <Image
+        style={burgerStyles.star2}
+        source={require('./assets/star1.png')}
+      />
+      <Image
+        style={burgerStyles.star3}
+        source={require('./assets/star1.png')}
+      />
       <Image source={require('./assets/hb2.png')} style={burgerStyles.main} />
       <Image source={require('./assets/hb1.png')} style={burgerStyles.upper} />
       <Text style={styles.foodName}>BURGER</Text>
-      <Text style={styles.price}>6$</Text>
+      <Text style={styles.price}>6 $</Text>
     </View>
   );
 }
 
+const foods = [
+  {
+    img: require('./assets/shutiao.png'),
+    price: 4,
+  },
+  {
+    img: require('./assets/coffee.png'),
+    price: 3,
+  },
+  {
+    img: require('./assets/hanbao.png'),
+    price: 6,
+  },
+];
+
 const App = () => {
   const [index, setIndex] = useState(0);
+  const [total, setTotal] = useState(0);
   const [routes] = useState([
-    {key: 'first', title: 'First'},
-    {key: 'second', title: 'Second'},
-    {key: 'third', title: 'Third'},
+    {key: 'first', title: 'Fire'},
+    {key: 'second', title: 'Coffee'},
+    {key: 'third', title: 'Burger'},
   ]);
 
   const initialLayout = {width: Dimensions.get('window').width};
@@ -258,6 +369,62 @@ const App = () => {
     third: Burger,
   });
 
+  const pos1 = useRef(new Animated.ValueXY()).current;
+  const pos2 = useRef(new Animated.ValueXY()).current;
+  const pos3 = useRef(new Animated.ValueXY()).current;
+
+  const [vis1, setVis1] = useState('none');
+  const [vis2, setVis2] = useState('none');
+  const [vis3, setVis3] = useState('none');
+
+  const setVis = [setVis1, setVis2, setVis3];
+  const pos = [pos1, pos2, pos3];
+
+  const [addedNum, setNum] = useState(0);
+  const [prev, setPrev] = useState<Animated.ValueXY>();
+
+  function onAddPress() {
+    setTotal(total + foods[index].price);
+    if (addedNum < 3) {
+      setVis[index]('flex');
+      const target = {x: -110, y: 120};
+
+      if (prev) {
+        const _target = {...target};
+        if (addedNum === 1) {
+          _target.y -= 30;
+        }
+
+        if (addedNum === 2) {
+          _target.x -= 60;
+          _target.y += 10;
+        }
+
+        Animated.spring(prev, {
+          toValue: _target,
+          useNativeDriver: true,
+        }).start();
+      }
+
+      if (addedNum === 1) {
+        target.x -= 50;
+        target.y += 10;
+      }
+
+      if (addedNum === 2) {
+        target.x += 30;
+        target.y += 20;
+      }
+
+      Animated.spring(pos[index], {
+        toValue: target,
+        useNativeDriver: true,
+      }).start();
+      setPrev(pos[index]);
+      setNum(addedNum + 1);
+    }
+  }
+
   return (
     <LinearGradient
       colors={['#F5F5F5', '#FFEDED']}
@@ -266,41 +433,33 @@ const App = () => {
       style={[styles.container]}>
       <LinearGradient colors={['#FD003C', '#FF5D79']} style={styles.header}>
         <View style={styles.menu}>
-          <Image source={suggestImg} style={styles.menuIcon} />
+          <Image source={IMG.suggest} style={styles.menuIcon} />
         </View>
         <View style={styles.menu}>
-          <Image source={burgerImg} style={styles.menuIcon} />
+          <Image source={IMG.burger} style={styles.menuIcon} />
         </View>
         <View style={styles.menu}>
-          <Image source={cupImg} style={[styles.menuIcon, {width: 25}]} />
+          <Image source={IMG.cup} style={[styles.menuIcon, {width: 25}]} />
         </View>
         <View style={styles.menu}>
-          <Image source={eatImg} style={styles.menuIcon} />
+          <Image source={IMG.eat} style={styles.menuIcon} />
         </View>
       </LinearGradient>
-      {/* <View>
-        <TabView
-          navigationState={{index, routes}}
-          renderScene={renderScene}
-          onIndexChange={setIndex}
-          initialLayout={initialLayout}
-          style={[styles.scrollViewContainer, {backgroundColor: 'red'}]}
-        />
-      </View> */}
 
-      <Image
-        source={require('./assets/add.png')}
-        style={styles.addButton}
-        onPress={Alert.alert(index + '')}
+      <TabView
+        navigationState={{index, routes}}
+        renderScene={renderScene}
+        renderTabBar={() => <View />}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+        style={styles.tabView}
       />
 
       <View style={styles.line} />
+
       <View style={styles.body}>
         <View style={styles.plateContainer}>
-          <Image source={plateImg} />
-          {/* <Image source={require('./assets/coffee.png')} /> */}
-          {/* <Image source={require('./assets/hanbao.png')} /> */}
-          {/* <Image source={require('./assets/shutiao.png')} /> */}
+          <Image source={IMG.plate} />
         </View>
         <View style={styles.mapContainer}>
           <Image
@@ -316,10 +475,51 @@ const App = () => {
           />
         </View>
       </View>
+
+      <Animated.Image
+        style={[
+          styles.food,
+          {
+            transform: [{translateX: pos1.x}, {translateY: pos1.y}],
+          },
+          {
+            display: vis1,
+          },
+        ]}
+        source={require('./assets/shutiao.png')}
+      />
+
+      <Animated.Image
+        style={[
+          styles.food,
+          {
+            transform: [{translateX: pos2.x}, {translateY: pos2.y}],
+          },
+          {
+            display: vis2,
+          },
+        ]}
+        source={require('./assets/coffee.png')}
+      />
+
+      <Animated.Image
+        style={[
+          styles.food,
+          {
+            transform: [{translateX: pos3.x}, {translateY: pos3.y}],
+          },
+          {
+            display: vis3,
+          },
+        ]}
+        source={require('./assets/hanbao.png')}
+      />
+
       <View style={styles.footer}>
         <View>
           <Text style={styles.coast}>
-            0<Text style={styles.unit}>$</Text>
+            {total}
+            <Text style={styles.unit}> $</Text>
           </Text>
         </View>
         <LinearGradient
@@ -328,14 +528,9 @@ const App = () => {
           <Text style={styles.payText}>Pay</Text>
         </LinearGradient>
       </View>
-      <TabView
-        navigationState={{index, routes}}
-        renderScene={renderScene}
-        renderTabBar={() => <View />}
-        onIndexChange={setIndex}
-        initialLayout={initialLayout}
-        style={styles.tabView}
-      />
+      <TouchableOpacity style={styles.addButton} onPress={() => onAddPress()}>
+        <Image source={require('./assets/add.png')} />
+      </TouchableOpacity>
     </LinearGradient>
   );
 };
